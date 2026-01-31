b84a1f8acf4804cc1606337451061c8323dcebfd
Jirka Vomlel <vomlel@utia.cas.cz>
2026-01-31 23:57:32 +0100

    Trust for other institutions and etc.

--- read-data.R
+++ read-data.R
@@ -2,6 +2,10 @@ library(haven)
 # A takhle z toho "vysochas" jmena a oznaceni sloupcu
 # column_names <- colnames(dataset)
 # variable_labels <- sapply(dataset, function(x) attr(x, "label"), simplify = "array")
+#
+# Attributes from the SAV file
+# attr(d[[1]]$w1_gen_trust, "label")
+# attr(d[[1]]$w1_gen_trust, "labels")
 
 ###########################################################################################################
 # Trust in the Constitutional Court 
@@ -26,7 +30,30 @@ fnames <- c("../CzechAttitudeBarometer/Vlna 1 (červen a červenec 2024)/PNS 240
             "../CzechAttitudeBarometer/Vlna 6 (červenec 2025)/PNS 2507 CoRe CAB W6_FINAL.sav", 
             "../CzechAttitudeBarometer/Vlna 7 (září 2025)/PNS 2509 CoRe CAB W7_FINAL.sav", 
             "../CzechAttitudeBarometer/Vlna 8 (říjen 2025)/PNS 2510 CoRe CAB W8_FINAL.sav")
+
+# General Trust (have 11 states)
+# names <- c("w1_gen_trust", NA, "w3_gen_trust", NA, "w5_gen_trust", NA, "w7_gen_trust", "w8_gen_trust")
+
+# President
+# names <- c("w1_pol_trust_1",	NA,	"w3_pol_trust_1",	NA,	"w5_pol_trust_1", "w6_pol_trust_1",	"w7_pol_trust_1", "w8_pol_trust_1")
+
+# Government
+# names <- c("w1_pol_trust_2",	NA,	"w3_pol_trust_2",	NA,	"w5_pol_trust_2", "w6_pol_trust_2",	"w7_pol_trust_2", "w8_pol_trust_2")
+
+# Chamber of Deputies of the Parliament
+# names <- c("w1_pol_trust_3",	NA,	"w3_pol_trust_3",	NA,	"w5_pol_trust_3", "w6_pol_trust_3",	"w7_pol_trust_3", "w8_pol_trust_3")
+
+# Constitutional court
 names <- c("w1_pol_trust_5",	NA,	"w3_pol_trust_5",	NA,	"w5_pol_trust_5", "w6_pol_trust_5",	"w7_pol_trust_5", "w8_pol_trust_5")
+
+# Army
+# names <- c("w1_pol_trust_7",	NA,	"w3_pol_trust_7",	NA,	"w5_pol_trust_7", "w6_pol_trust_7",	"w7_pol_trust_7", "w8_pol_trust_7")
+
+# Public media (e.g., Czech Television, Czech Radio)
+# names <- c("w1_pol_trust_8",	NA,	"w3_pol_trust_8",	NA,	"w5_pol_trust_8", "w6_pol_trust_8",	"w7_pol_trust_8", "w8_pol_trust_8")
+
+# Scientists
+# names <- c("w1_pol_trust_9",	NA,	"w3_pol_trust_9",	NA,	"w5_pol_trust_9", "w6_pol_trust_9",	"w7_pol_trust_9", "w8_pol_trust_9")
 
 d <- list()
 dates <- list()
@@ -54,9 +81,14 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 
+# Set locale to English for dates
+Sys.setlocale("LC_TIME", "C")
+
+
 # Define states and colors
 states <- c("Strong trust", "Rather trust", "Rather distrust", "Strong distrust")
-col <- colorRampPalette(c("blue", "cyan", "yellow", "orange", "red"))(4)
+# states <- 0:10
+col <- colorRampPalette(c("blue", "cyan", "yellow", "orange", "red"))(length(states))
 
 # Extract non-missing tables and remove value 98
 df_long <- lapply(seq_along(tables), function(i) {
@@ -87,6 +119,7 @@ ggplot(df_long, aes(x = date, y = percentage, fill = category)) +
             size = 3) +  # Add percentage labels
   scale_fill_manual(values = col) +  # Custom color scheme
   scale_color_manual(values = c("white", "black", "black", "black"), guide = "none") +  # Text colors
+  # scale_color_manual(values = c("white", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"), guide = "none") +  # Text colors
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
   labs(
     title = "Trend Over Time",
